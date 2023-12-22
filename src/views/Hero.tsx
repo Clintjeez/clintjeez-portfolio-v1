4f5821419e2e459f7757a95b172069b5df861487
clintjeez <clintjeez@gmail.com>
2023-12-23 00:23:41 +0100

    image error fix

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -1,31 +1,8 @@
 "use client"
 import React from 'react'
-import ParticleImage, { ParticleOptions,
-    Vector,
-    forces,
-    ParticleForce } from "react-particle-image";
-import clintImg from '../../public/images/hero-img.jpeg'
 import Image from 'next/image';
 
-const particleOptions: ParticleOptions = {
-    filter: ({ x, y, image }) => {
-      // Get pixel
-      const pixel = image.get(x, y);
-      // Make a particle for this pixel if blue > 50 (range 0-255)
-      return pixel.b > 50;
-    },
-    color: ({ x, y, image }) => "#61dafb",
-    radius: () => Math.random() * 1.5 + 0.5,
-    mass: () => 40,
-    friction: () => 0.15,
-    initialPosition: ({ canvasDimensions }) => {
-      return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
-    }
-  };
 
-  const motionForce = (x: number, y: number): ParticleForce => {
-    return forces.disturbance(x, y, 5);
-  };
 
 const Hero = () => {
    
